4a7c7c25bdd98de6188d67a817e7db91b62cd273
Pranay Rayke <120271377+Pranay-Rayke@users.noreply.github.com>
2023-07-24 21:33:59 +0530

    created a submission

--- script.js
+++ script.js
@@ -16,10 +16,17 @@ const library = [
   },
 ];
 
-const numberOfBooksRead = () => {
+const numberOfBooksRead = (library) => {
   // write your code here
+	let count = 0;
+  for (let i = 0; i < library.length; i++) {
+    const book = library[i];
+    if (book.readingStatus === true) {
+      count++;
+    }
+  }
+  return count;
+}
 };
-
-// Do not change the code below
 
 alert(numberOfBooksRead());

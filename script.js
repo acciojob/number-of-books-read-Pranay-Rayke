a2cfe621dd1762ed1dfb7302fc2b5cabadb3cdd3
Pranay Rayke <120271377+Pranay-Rayke@users.noreply.github.com>
2023-07-24 21:58:24 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,34 @@
 
+const library = [
+  {
+    author: "Bill Gates",
+    title: "The Road Ahead",
+    readingStatus: true,
+  },
+  {
+    author: "Steve Jobs",
+    title: "Walter Isaacson",
+    readingStatus: true,
+  },
+  {
+    author: "Suzanne Collins",
+    title: "Mockingjay: The Final Book of The Hunger Games",
+    readingStatus: false,
+  },
+];
+
+const numberOfBooksRead = () =>{
+  // write your code here
+	let count = 0;
+  for (let i = 0; i < library.length; i++) {
+    const book = library[i];
+    if (book.readingStatus == true) {
+      count++;
+    }
+  }
+  return count;
+};
+
+// Do not change the code below
+
+alert(numberOfBooksRead());
